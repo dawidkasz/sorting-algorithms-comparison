00a35230df7fd990edf19c26bc14590f76a10721
dawidkasz <dkaszynski04@gmail.com>
2022-03-24 12:23:32 +0100

    Add Bubble and Selection sort

--- src/sorting.py
+++ src/sorting.py
@@ -2,11 +2,28 @@ from src.utils import get_median_of_three, split_through_pivot, merge_arrays
 
 
 def bubble_sort(array):
-    pass
+    array_length = len(array)
+    if array_length < 2:
+        return array
+
+    for i in range(array_length - 1):
+        for j in range(0, array_length - i - 1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+    return array
 
 
 def selection_sort(array):
-    pass
+    array_length = len(array)
+    if array_length < 2:
+        return array
+
+    for i in range(array_length - 1):
+        temp = array[i::]
+        min_val = min(temp)
+        min_index = temp.index(min_val) + i
+        array[i], array[min_index] = array[min_index], array[i]
+    return array
 
 
 def merge_sort(array):
