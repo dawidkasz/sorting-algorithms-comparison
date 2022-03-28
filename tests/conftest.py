f6975eb50c6390d38a02abc6b95a48a34b47118a
dawidkasz <dkaszynski04@gmail.com>
2022-03-28 22:59:13 +0200

    Cleanup

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,19 +4,28 @@ import pytest
 
 def pytest_configure():
     pytest.test_case = 'test_case'
-    pytest.sorting_test_cases = ['test_case_numbers',
+    pytest.sorting_test_cases = ['test_case_numbers_1',
+                                 'test_case_numbers_2',
                                  'test_case_numbers_negative',
                                  'test_case_words',
                                  'test_case_sorted',
+                                 'test_case_reverse_sorted',
                                  'test_case_empty']
     pytest.pan_tadeusz_relative_path = 'data/pan-tadeusz.txt'
 
 
 @pytest.fixture
-def test_case_numbers():
+def test_case_numbers_1():
     return (
         [9, 7, 2, 4, 11, 7, 1, 4, 5, 14, 8, 154, 21, 33, 2, 18],
         [1, 2, 2, 4, 4, 5, 7, 7, 8, 9, 11, 14, 18, 21, 33, 154]
+    )
+
+@pytest.fixture
+def test_case_numbers_2():
+    return (
+        [34, 3, 29, 26, 9, 40, 5, 12, 3, 37, 7, 32, 42, 44, 3, 21, 35, 18, 42, 45],
+        [3, 3, 3, 5, 7, 9, 12, 18, 21, 26, 29, 32, 34, 35, 37, 40, 42, 42, 44, 45]
     )
 
 
@@ -41,6 +50,13 @@ def test_case_sorted():
     return (
         [1, 2, 3, 4, 5],
         [1, 2, 3, 4, 5]
+    )
+
+@pytest.fixture
+def test_case_reverse_sorted():
+    return (
+        [7, 4, 2, 0, -3],
+        [-3, 0, 2, 4, 7]
     )
 
 
